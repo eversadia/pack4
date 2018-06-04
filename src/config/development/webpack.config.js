82125086020face65eba1db5e4f6dc96b0b89ed0
evertech <ever@techasiaporta.com>
2018-06-04 10:20:39 +0800

    partial update

--- src/config/development/webpack.config.js
+++ src/config/development/webpack.config.js
@@ -1,44 +1,65 @@
 const path = require( 'path' )
-const HtmlWebPackPlugin = require( "html-webpack-plugin" );
-const MiniCssExtractPlugin = require( "mini-css-extract-plugin" );
+const HtmlWebPackPlugin = require( "html-webpack-plugin" )
+const MiniCssExtractPlugin = require( "mini-css-extract-plugin" )
+const UglifyJSPlugin = require( 'uglifyjs-webpack-plugin' )
+const config = {}
 
-module.exports = {
-  entry: {
-    app: './src/index.js'
-  },
-  output: {
-    path: path.resolve( __dirname, '../../../build' ),
-    filename: '[name].js?[hash]'
-  },
-  module: {
-    rules: [ {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader"
+config.entry = {
+  app: './src/index.js'
+}
+
+config.output = {
+  path: path.resolve( __dirname, '../../../build' ),
+  filename: '[name].js?[hash]'
+}
+
+config.module = {
+  rules: [ {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader"
+    }
+  }, {
+    test: /\.html$/,
+    use: [ {
+      loader: "html-loader",
+      options: {
+        minimize: true
       }
-    }, {
-      test: /\.html$/,
-      use: [ {
-        loader: "html-loader",
-        options: {
-          minimize: true
-        }
-      } ]
-    }, {
-      test: /\.css$/,
-      use: [ MiniCssExtractPlugin.loader, "css-loader" ]
     } ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin( {
+  }, {
+    test: /\.css$/,
+    use: [ MiniCssExtractPlugin.loader, "css-loader" ]
+  } ]
+}
 
-      template: "./src/template/index.html",
-      filename: "./index.html"
-    } ),
-    new MiniCssExtractPlugin( {
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    } )
-  ]
-};
+config.plugins = [
+  new HtmlWebPackPlugin( {
+
+    template: "./src/template/index.html",
+    filename: "./index.html"
+  } ),
+  new MiniCssExtractPlugin( {
+    filename: "[name].css",
+    chunkFilename: "[id].css"
+  } )
+]
+
+config.mode = 'development'
+config.target = 'web'
+
+config.devServer = {
+  open: true,
+  compress: true,
+  port: 9000
+}
+
+config.resolve = {
+  alias: {
+    src: path.resolve( __dirname, 'src/' ),
+    utils: path.resolve( __dirname, 'src/utils/' )
+  }
+}
+
+module.exports = config
