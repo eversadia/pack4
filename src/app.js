3c613159aa2db32f66817dee8b178eb629a277b7
evertech <ever@techasiaporta.com>
2018-07-02 14:16:23 +0800

    add react router dom

--- src/app.js
+++ src/app.js
@@ -1,14 +1,93 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import React, {
+  Component
+} from "react"
+import ReactDOM from "react-dom"
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <p>React here!</p>
     </div>
-  );
-};
+  )
+}
 
 export default App;
 
-ReactDOM.render(<App />, document.getElementById("app"));
+const PreventingTransitionsExample = () => (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Form</Link>
+        </li>
+        <li>
+          <Link to="/one">One</Link>
+        </li>
+        <li>
+          <Link to="/two">Two</Link>
+        </li>
+      </ul>
+      <Route path="/" exact component={Form} />
+      <Route path="/one" render={() => <h3>One</h3>} />
+      <Route path="/two" render={() => <h3>Two</h3>} />
+    </div>
+  </Router>
+);
+
+class Form extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
+
+    this.state.isBlocking = false
+
+  }
+
+  render() {
+    const {
+      isBlocking
+    } = this.state;
+
+    return (
+      <form
+        onSubmit={event => {
+          event.preventDefault();
+          event.target.reset();
+          this.setState({
+            isBlocking: false
+          });
+        }}
+      >
+        
+        <p>
+          Blocking?{" "}
+          {isBlocking ? "Yes, click a link or the back button" : "Nope"}
+        </p>
+
+        <p>
+          <input
+            size="50"
+            placeholder="type something to block transitions"
+            onChange={event => {
+              this.setState({
+                isBlocking: event.target.value.length > 0
+              });
+            }}
+          />
+        </p>
+
+        <p>
+          <button>Submit to stop blocking</button>
+        </p>
+      </form>
+    );
+  }
+}
+
+ReactDOM.render( <PreventingTransitionsExample />, document.getElementById( "app" ) )
